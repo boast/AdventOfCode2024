21366d637b3d82775f21da4754fd88271f4f841b
Dave Thalmann <david.thalmann@gmail.com>
2024-12-08 22:14:50 +0100

    Day03: Do part1 with regex, don't do part2 with regex - use a scanner approach.

---
+++ Days/Day03.cs
@@ -0,0 +1,106 @@
+﻿using AdventOfCode2024.Utils;
+
+namespace AdventOfCode2024.Days;
+
+public static class Day03
+{
+    public static void Solve()
+    {
+        var lines = InputReader.ReadDay("03");
+        Part1(lines);
+        Part2(lines);
+    }
+
+    private static void Part1(List<string> lines)
+    {
+        var input = string.Join("", lines);
+        var sum = 0;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (!CheckNext(i, input, "mul("))
+            {
+                continue;
+            }
+            i += 4;
+            
+            int firstDigit = 0, secondDigit = 0;
+            while (i < input.Length && char.IsDigit(input[i]))
+            {
+                firstDigit = firstDigit * 10 + (input[i++] - '0');
+            }
+
+            if (CheckNext(i, input, ","))
+            {
+                i++;
+                while (i < input.Length && char.IsDigit(input[i]))
+                {
+                    secondDigit = secondDigit * 10 + (input[i++] - '0');
+                }
+            }
+
+            if (CheckNext(i, input, ")"))
+            {
+                sum += firstDigit * secondDigit;
+            }
+        }
+
+        Console.WriteLine($"Part 1: {sum}");
+    }
+
+    private static void Part2(List<string> lines)
+    {
+        var input = string.Join("", lines);
+        var sum = 0;
+        var doing = true;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (CheckNext(i, input, "do()"))
+            {
+                doing = true;
+                i += 3;
+                continue;
+            }
+            if (CheckNext(i, input, "don't()"))
+            {
+                doing = false;
+                i += 6;
+                continue;
+            }
+
+            if (!doing || !CheckNext(i, input, "mul("))
+            {
+                continue;
+            }
+            i += 4;
+            
+            int firstDigit = 0, secondDigit = 0;
+            while (i < input.Length && char.IsDigit(input[i]))
+            {
+                firstDigit = firstDigit * 10 + (input[i++] - '0');
+            }
+
+            if (CheckNext(i, input, ","))
+            {
+                i++;
+                while (i < input.Length && char.IsDigit(input[i]))
+                {
+                    secondDigit = secondDigit * 10 + (input[i++] - '0');
+                }
+            }
+
+            if (CheckNext(i, input, ")"))
+            {
+                sum += firstDigit * secondDigit;
+            }
+        }
+
+        Console.WriteLine($"Part 2: {sum}");
+    }
+
+    private static bool CheckNext(int i, string input, string text)
+    {
+        return i + text.Length <= input.Length && input.Substring(i, text.Length) == text;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,41 @@
-﻿using AdventOfCode2024.Days;
+﻿using System.Reflection;
 
-Day02.Solve();
+var dayClasses = Assembly.GetExecutingAssembly()
+    .GetTypes()
+    .Where(t => t.Namespace == "AdventOfCode2024.Days" && t.Name.StartsWith("Day"))
+    .OrderByDescending(t => t.Name)
+    .ToList();
+
+if (dayClasses.Count != 0)
+{
+    var latestDayClass = dayClasses.First();
+    var solveMethod = latestDayClass.GetMethod("Solve");
+
+    if (solveMethod != null)
+    {
+        WriteHeader(latestDayClass.Name);
+
+        solveMethod.Invoke(null, null);
+    }
+    else
+    {
+        Console.WriteLine("Solve method not found in the latest day class.");
+    }
+}
+else
+{
+    Console.WriteLine("No day classes found.");
+}
+
+return;
+
+void WriteHeader(string latestDayClassName)
+{
+    var dayNumber = latestDayClassName[3..];
+    var header = $"=== Day {dayNumber} ===";
+        
+    Console.WriteLine(new string('=', header.Length));
+    Console.WriteLine(header);
+    Console.WriteLine(new string('=', header.Length));
+    Console.WriteLine();
+}
